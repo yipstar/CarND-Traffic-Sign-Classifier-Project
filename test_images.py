69059202d81b89125155e975a691cfa03627b0d9
Gianni Jacklone <gianni.jacklone@gmail.com>
2017-01-02 13:46:16 -0800

    update test images loader

--- test_images.py
+++ test_images.py
@@ -1,7 +1,23 @@
-import cv
+from PIL import Image
 import numpy as np
+import os, os.path
 
-x = cv.LoadImageM('test_images/children_28.jpg')
-im = np.asarray(x)
+def get_test_data():
 
-print(im.shape)
+    X_test_images = []
+    y_test_images = []
+
+    for f in os.listdir("test_images"):
+        parts = os.path.splitext(f)
+        label = parts[0].split("_")[1]
+
+        image = Image.open("test_images/{0}".format(f))
+        data = np.asarray(image, dtype="int32" )
+
+        y_test_images.append(label)
+        X_test_images.append(data)
+
+        print(y_test_images)
+        print(X_test_images)
+
+    return (X_test_images, y_test_images)
